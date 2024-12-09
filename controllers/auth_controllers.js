64a80392c30cc8d0646c52fd01352c5a69fc7d94
Nandini242003 <135803262+Nandini242003@users.noreply.github.com>
2024-12-09 12:58:28 +0530

    Update auth_controllers.js

--- controllers/auth_controllers.js
+++ controllers/auth_controllers.js
@@ -2,7 +2,7 @@ import {Client,Account,ID,Functions} from "appwrite"
 
 const client = new Client()
 .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-.setProject('<YOUR-PROJECT-ID>')              // Your project ID
+.setProject('67514d45003e42bd56c8')              // Your project ID
 
 const account = new Account(client);
 
